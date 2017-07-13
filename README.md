7602d27fe8f051169c570b273dcd73983e44290e
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-07-13 11:04:19 +0200

    README updates

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# roads-osm
-Extract roads from OpenStreet Map
+# Extract roads from Open Street Map (OSM)
+
+## Code
+
+This repository includes a bash script to extract roads from Open
+Street Map (OSM). Computation relies on
+[`osmtools`](https://gitlab.com/osm-c-tools/osmctools) and
+[`GDAL/OGR`](<http://gdal.org>) utilities.
+
+## Results
+
+The code produces a raster of African roads extracted from Open Street
+Map (OSM). The raster includes all types of roads (highway=*) provided
+by OSM. The raster of roads has the following characteristics:
+
+Date: July 2017. Resolution: 30m. Size: 568.56 MB. Format: GeoTIFF. Data type:
+Byte. No-data value: 255. Projection: EPSG:102022 (Africa Albers Equal
+Area Conic).
+
+## Download
+
+The raster of African roads is available on Figshare: \[doi:
+[10.6084/m9.figshare.5202814](https://doi.org/10.6084/m9.figshare.5202814.v1)\]
+
+
--- roads_osm.sh
+++ roads_osm.sh
@@ -12,7 +12,7 @@
 # Variables
 #proj="EPSG:102022" # Africa_Albers_Equal_Area_Conic, see http://epsg.io
 #This epsg code is not recognized by gdal so we use WKT definition
-proj="./proj.prj" # Path to file with Well Known Text (WKT) projection definition
+proj="../proj.prj" # Path to file with Well Known Text (WKT) projection definition
 resolution=30 # 30m resolution
 
 # ===========================
@@ -22,12 +22,16 @@ resolution=30 # 30m resolution
 # Message
 echo "Roads from OSM\n"
 
+# New directory for results
+mkdir -p results
+cd results
+
 # Download OSM data from Geofabrik
 url="http://download.geofabrik.de/africa-latest.osm.pbf"
 wget -O country.osm.pbf $url
 osmconvert country.osm.pbf -o=country.o5m
 
-# All roads for Christelle
+# All roads
 osmfilter country.o5m --keep='highway=*' > all_roads.osm
 ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
         -sql 'SELECT osm_id, name, highway FROM lines WHERE highway IS NOT NULL' \
@@ -38,9 +42,9 @@ ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
         -lco ENCODING=UTF-8 all_roads_proj.shp all_roads.shp
 
 # Rasterize
-resolution=250
 gdal_rasterize -tap -burn 1 \
                -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" -ot Byte \
                -a_nodata 255 \
                -tr $resolution $resolution -l all_roads_proj all_roads_proj.shp all_roads.tif
 
+# End
