28a72e8bff8a2e07a409d3f6620c7eb587a46838
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-09-20 11:26:45 +0200

    Minor changes

--- roads_osm.py
+++ roads_osm.py
@@ -54,47 +54,48 @@ def roads_osm(planet, area, extent, proj, res):
 
     # New directory for results
     results_dir = "results_" + area
-    print("Create directory: " + results_dir)
+    # print("Create directory: " + results_dir)
     os.makedirs(results_dir)
     os.chdir(results_dir)
 
     # Call to osmconvert with box
-    box = ",".join(map(str, extent))
-    print("Convert OSM data with box: " + box)
-    os.system("osmconvert " + planet + " -b=" + box + " -o=area.o5m -v")
+    # box = ",".join(map(str, extent))
+    # print("Convert OSM data with box: " + box)
+    # os.system("osmconvert " + planet + " -b=" + box + " -o=area.o5m -v")
 
     # Resolution as string
     res_str = str(res) + " " + str(res)
 
     # Extract roads
-    print("Extract roads from OSM data")
-    os.system("osmfilter area.o5m --keep='highway=*' -o=all_roads.osm -v")
-    cmd = "ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
-           -sql 'SELECT osm_id, name, highway FROM lines \
-           WHERE highway IS NOT NULL' \
-           -lco ENCODING=UTF-8 all_roads.shp all_roads.osm"
-    os.system(cmd)
+    # print("Extract roads from OSM data")
+    # os.system("osmfilter area.o5m --keep='highway=*' -o=all_roads.osm -v")
+    # cmd = "ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
+    #        -sql 'SELECT osm_id, name, highway FROM lines \
+    #        WHERE highway IS NOT NULL' \
+    #        -lco ENCODING=UTF-8 all_roads.shp all_roads.osm"
+    # os.system(cmd)
 
     # Reproject
-    print("Reproject road vector data")
-    os.system("ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs " + proj + " -f 'ESRI Shapefile' \
-               -progress \
-               -lco ENCODING=UTF-8 all_roads_proj.shp all_roads.shp")
+    # print("Reproject road vector data")
+    # os.system("ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs " + proj + " -f 'ESRI Shapefile' \
+    #            -progress \
+    #            -lco ENCODING=UTF-8 all_roads_proj.shp all_roads.shp")
 
     # Rasterize
     print("Rasterize road vector data")
     cmd = "gdal_rasterize -tap -burn 1 \
-           -co 'COMPRESS=LZW' -co 'PREDICTOR=2' -co 'BIGTIFF=YES' -ot Byte \
+           -co 'COMPRESS=LZW' -co 'PREDICTOR=2' -co 'BIGTIFF=YES' \
+           -ot Byte \
            -a_nodata 255 \
            -tr " + res_str + " \
            -l all_roads_proj all_roads_proj.shp all_roads.tif"
     os.system(cmd)
 
 # Loop on areas of interest
-for i in range(5):
-    i = 1
-    planet = os.path.join(owd, "planet-latest.osm.pbf")
-    projection = os.path.join(owd, proj[i])
-    roads_osm(planet, area[i], extent[i], projection, 30)
+# for i in range(5):
+i = 2
+planet = os.path.join(owd, "planet-latest.osm.pbf")
+projection = os.path.join(owd, proj[i])
+roads_osm(planet, area[i], extent[i], projection, 30)
 
 # End
